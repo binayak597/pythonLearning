8e1d089ad2b23e254b02efd224055e6e970172a1
Binayak Mukherjee <binayak.mukherjee9640@gmail.com>
2024-10-20 12:43:19 +0530

    complete chapter04 concept and practice set

---
+++ chapter04/02_list_methods.py
@@ -0,0 +1,24 @@
+friends = ["Apple", "Orange", 5, 345.06, False, "Aakash", "Rohan"]
+print(friends)
+friends.append("Binayak")
+print(friends)
+
+l1 = [1, 34,62, 2, 6, 11]
+
+l1.sort()
+
+print(l1)
+
+l1.reverse()
+
+print(l1)
+
+l1.insert(2, 333333) # Insert 333333 at index 2
+
+print(l1)
+value = l1.pop()
+print(value)
+print(l1)
+
+result = l1.remove(21)
+print(l1)
---
+++ chapter04_PS/problem3.py
@@ -0,0 +1,6 @@
+# Check that a tuple type cannot be changed in python. 
+
+
+a = (34, 234, "Binayak")
+
+a[2] = "Chintu"
